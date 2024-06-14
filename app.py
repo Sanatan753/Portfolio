83b3449eca4514bc3230155161a2223c019eef54
Sanatan753 <sanatankisku753@gmail.com>
2024-06-14 13:26:16 +0530

    Update python Routing

--- app.py
+++ app.py
@@ -1,25 +1,29 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, render_template
 import pickle
 import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
 
 app = Flask(__name__)
 
 # Load your model
-with open('model.pkl', 'rb') as f:
+with open('linear_regression_model.pkl', 'rb') as f:
     model = pickle.load(f)
 
 @app.route('/')
 def home():
-    return render_template('project2.html')
+    return render_template('index.html')
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    data = [float(x) for x in request.form.values()]
-    prediction = model.predict([np.array(data)])
-    output = round(prediction[0], 2)
-    return render_template('project2.html', prediction_text=f'Predicted Value: {output}')
+    if request.method == 'POST':
+        try:
+            data = [float(x) for x in request.form.values()]
+            prediction = model.predict([np.array(data)])
+            output = round(prediction[0], 2)
+            return render_template('project2.html', prediction_text=f'Predicted Value: {output}')
+        except Exception as e:
+            return render_template('project2.html', prediction_text=f'Error: {e}')
+    else:
+        return render_template('project2.html', prediction_text='Please submit a POST request.')
 
 if __name__ == "__main__":
     app.run(debug=True)
