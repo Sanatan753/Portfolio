409e280f0d6615c2cab3f39a1b36fae203954fa7
Sanatan753 <sanatankisku753@gmail.com>
2024-06-15 11:10:02 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,13 @@
+console.error("Why the FUCK YOU are in the Console ");
+
+var website="https://tinywow.com/";
+var name="Sanatan"
+console.log(website);
+
+let appels = 1;
+const height = "6'0\"";
+
+if ( name == "Sanatan"){
+    let apples = 5;
+    console.log("I have",apples,"apples") 
+}
