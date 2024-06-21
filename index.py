e805f1fdd7b23e6bccd40cd89f35a096259922b7
Sanatan753 <sanatankisku753@gmail.com>
2024-06-21 09:03:12 +0530

    New version

---
+++ index.py
@@ -0,0 +1,29 @@
+from flask import Flask, request, render_template
+import pickle
+import numpy as np
+
+app = Flask(__name__)
+
+# Load your model
+with open('linear_regression_model.pkl', 'rb') as f:
+    model = pickle.load(f)
+
+@app.route('/')
+def home():
+    return render_template('Home.ejs')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        try:
+            data = [float(x) for x in request.form.values()]
+            prediction = model.predict([np.array(data)])
+            output = round(prediction[0], 2)
+            return render_template('project2.html', prediction_text=f'Predicted Value: {output}')
+        except Exception as e:
+            return render_template('project2.html', prediction_text=f'Error: {e}')
+    else:
+        return render_template('project2.html', prediction_text='Please submit a POST request.')
+
+if __name__ == "__main__":
+    app.run(debug=True)
